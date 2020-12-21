9b47d204779803926cc78a2f905ff8f192fb1a65
Kunal <kunaljain1013@hotmail.com>
2020-12-21 16:05:41 +0530

    update configs

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from logging import DEBUG
 
 
@@ -5,3 +6,10 @@ API_USER_NAME = 'user'
 API_USER_PASS = 'pass'
 
 LOG_LEVEL = DEBUG
+
+NOTIF_URL = 'https://2055430c-dd72-485c-a364-74be856d1875.mock.pstmn.io/send-notification'
+
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_NAME = os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST')
